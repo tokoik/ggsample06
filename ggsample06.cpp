1d46ed647e3d6ec645f8a0752f66788bdc68a539
Kohe Tokoi <tokoik@gmail.com>
2020-02-16 11:44:29 +0900

    remove GgApplication.h and add Window.h

--- ggsample06.cpp
+++ ggsample06.cpp
@@ -1,5 +1,5 @@
 ﻿// ウィンドウ関連の処理
-#include "GgApplication.h"
+#include "Window.h"
 
 // シェーダー関連の処理
 #include "shader.h"
@@ -13,7 +13,7 @@ const double cycle(5.0);
 //
 // アプリケーションの実行
 //
-void GgApplication::run()
+void app()
 {
   // ウィンドウを作成する
   Window window("ggsample06");
